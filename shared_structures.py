2c5fc8444d017b79fccec582fadb9ebbd5262141
Loriynne Duarte <loriynne@gmail.com>
2026-06-22 11:47:07 -0700

    Shared_structures - Juan Base

--- shared_structures.py
+++ shared_structures.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class RequestPacket:
     request_id: int         
-    arrival_time: float      # Virtual time in seconds
-    actual_blocks: int      # real count of KV-cache block 
+    arrival_time: float        # Virtual time in seconds
+    actual_blocks: int        # real count of KV-cache block 
     predicted_mu: float      #  Predicted block count with error 
-    predicted_sigma: float   # Uncertainty of the prediction - standard deviation 
-    wait_time: float = 0.0   # Seconds waited in the queue
+    predicted_sigma: float    # Uncertainty of the prediction - standard deviation 
+    wait_time: float = 0.0    # Seconds waited in the queue
